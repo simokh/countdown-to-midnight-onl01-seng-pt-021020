05d7798c369df6c489ffac873599271cbc94b0b4
Simo Khouili <m.simokhouili@gmail.com>
2020-02-15 14:22:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
-
 def countdown
+
+  count = 10
+     until count == -1 do
+
+    puts "#{count} SECOND(S)!"
+      count -= 1
+      sleep 5
+    end
+    'happy new year'.upcase
 end
+
+countdown
